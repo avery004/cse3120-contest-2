15582d5d839e6bb3b4896bc03652075d461482d1
Avery Mackey <126718844+avery004@users.noreply.github.com>
2026-04-19 20:22:04 -0400

    Added lib includes

--- tempest.asm
+++ tempest.asm
@@ -1,10 +1,21 @@
 ; tempest.asm
-
+; CSE 3120 Contest 2
+; Project: basic Tempest-style game in MASM
 .386
 .model flat, stdcall
 .stack 4096
 
 INCLUDE Irvine32.inc
+; Irvine32.inc supplies course helpers and usually includes SmallWin.inc.
+; Keep Win32 library links here for later graphics and window calls.
+INCLUDELIB Irvine32.lib
+INCLUDELIB kernel32.lib
+INCLUDELIB user32.lib
+INCLUDELIB gdi32.lib
+
+; kernel32: process startup and system calls.
+; user32: windows, messages, and keyboard input.
+; gdi32: lines, pens, brushes, and back buffers.
 
 .code
 main PROC
