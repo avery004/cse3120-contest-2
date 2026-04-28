f1cd3c9015cc698fba7ca794834b87504bb7de78
Avery Mackey <126718844+avery004@users.noreply.github.com>
2026-04-27 23:51:45 -0400

    Draw score and lives HUD

--- tempest.asm
+++ tempest.asm
@@ -115,6 +115,8 @@ titleText1  BYTE "MASM TEMPEST",0
 titleText2  BYTE "PRESS SPACE TO START",0
 gameOverText1 BYTE "GAME OVER",0
 gameOverText2 BYTE "PRESS ENTER TO RESTART",0
+hudFormat  BYTE "SCORE %u  LIVES %u",0
+hudBuffer  BYTE 32 DUP(0)
 ; Stores future window identifiers.
 ; className is used when registering the class.
 ; windowTitle appears in the title bar.
@@ -254,18 +256,20 @@ WndProc PROC,
     call DrawEnemies
     INVOKE SelectObject, paintData.hdc, oldPen
     INVOKE DeleteObject, testPen
-    ; Draw state prompts with a transparent text background.
+    ; Draw the HUD and state prompts with a transparent text background.
     INVOKE SetBkMode, paintData.hdc, TRANSPARENT
+    INVOKE SetTextColor, paintData.hdc, 0000FFFFh
+    INVOKE wsprintf, ADDR hudBuffer, ADDR hudFormat, score, lives
+    mov ecx, eax
+    INVOKE TextOut, paintData.hdc, 16, 16, ADDR hudBuffer, ecx
     cmp gameState, STATE_TITLE
     jne check_game_over
-    INVOKE SetTextColor, paintData.hdc, 0000FFFFh
     INVOKE TextOut, paintData.hdc, 320, 24, ADDR titleText1, LENGTHOF titleText1 - 1
     INVOKE TextOut, paintData.hdc, 250, 52, ADDR titleText2, LENGTHOF titleText2 - 1
     jmp finish_paint
 check_game_over:
     cmp gameState, STATE_GAME_OVER
     jne finish_paint
-    INVOKE SetTextColor, paintData.hdc, 0000FFFFh
     INVOKE TextOut, paintData.hdc, 332, 24, ADDR gameOverText1, LENGTHOF gameOverText1 - 1
     INVOKE TextOut, paintData.hdc, 230, 52, ADDR gameOverText2, LENGTHOF gameOverText2 - 1
 finish_paint:
