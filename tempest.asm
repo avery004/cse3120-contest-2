4062763223a4761a3955c1484fae2ebc7d804398
Avery Mackey <126718844+avery004@users.noreply.github.com>
2026-04-28 08:16:53 -0400

    Final pass through fixing all major bugs

--- tempest.asm
+++ tempest.asm
@@ -20,13 +20,19 @@ CENTER_Y    EQU WINDOW_HEIGHT / 2
 LANE_COUNT  EQU 12
 NEAR_RADIUS EQU 220
 FAR_RADIUS  EQU 90
+DEPTH_RANGE EQU NEAR_RADIUS - FAR_RADIUS
 PLAYER_COLOR       EQU 0000FF00h
+SHOT_COLOR  EQU 000000FFh
+ENEMY_COLOR EQU 0000FFFFh
 PLAYER_MARKER_SIZE EQU 14
 PLAYER_PEN_WIDTH   EQU 2
 PLAYER_START_LANE  EQU 0
 MAX_SHOTS          EQU 6
 MAX_ENEMIES        EQU 6
-SPAWN_TICKS        EQU 45
+SPAWN_TICKS        EQU 240
+MIN_SPAWN_TICKS    EQU 40
+SPAWN_STEP_SCORE   EQU 1000
+SPAWN_STEP_TICKS   EQU 40
 START_SCORE        EQU 0
 INITIAL_LIVES      EQU 3
 ENEMY_SCORE        EQU 100
@@ -50,17 +56,124 @@ STATE_GAME_OVER    EQU 3
 ; Game state values keep later input and timer checks simple.
 ; Title remains the default until an explicit start action.
 
-INCLUDE Irvine32.inc
-; Irvine32.inc supplies course helpers and usually includes SmallWin.inc.
-; Keep Win32 library links here for later graphics and window calls.
-INCLUDELIB Irvine32.lib
+option casemap:none
+
 INCLUDELIB kernel32.lib
 INCLUDELIB user32.lib
 INCLUDELIB gdi32.lib
 
-; kernel32: process startup and system calls.
-; user32: windows, messages, and keyboard input.
-; gdi32: lines, pens, brushes, and back buffers.
+NULL            EQU 0
+TRUE            EQU 1
+
+CS_VREDRAW      EQU 0001h
+CS_HREDRAW      EQU 0002h
+
+IDC_ARROW       EQU 32512
+CW_USEDEFAULT   EQU 80000000h
+
+WS_OVERLAPPEDWINDOW EQU 00CF0000h
+
+WM_CREATE       EQU 0001h
+WM_DESTROY      EQU 0002h
+WM_PAINT        EQU 000Fh
+WM_KEYDOWN      EQU 0100h
+WM_TIMER        EQU 0113h
+
+VK_RETURN       EQU 0Dh
+VK_ESCAPE       EQU 1Bh
+VK_SPACE        EQU 20h
+VK_LEFT         EQU 25h
+VK_RIGHT        EQU 27h
+
+BLACK_BRUSH     EQU 4
+PS_SOLID        EQU 0
+TRANSPARENT     EQU 1
+OPAQUE          EQU 2
+
+POINT STRUCT
+    x DWORD ?
+    y DWORD ?
+POINT ENDS
+
+RECT STRUCT
+    left   DWORD ?
+    top    DWORD ?
+    right  DWORD ?
+    bottom DWORD ?
+RECT ENDS
+
+MSG STRUCT
+    hwnd    DWORD ?
+    message DWORD ?
+    wParam  DWORD ?
+    lParam  DWORD ?
+    time    DWORD ?
+    pt      POINT <>
+MSG ENDS
+
+WNDCLASS STRUCT
+    style         DWORD ?
+    lpfnWndProc   DWORD ?
+    cbClsExtra    DWORD ?
+    cbWndExtra    DWORD ?
+    hInstance     DWORD ?
+    hIcon         DWORD ?
+    hCursor       DWORD ?
+    hbrBackground DWORD ?
+    lpszMenuName  DWORD ?
+    lpszClassName DWORD ?
+WNDCLASS ENDS
+
+PAINTSTRUCT STRUCT
+    hdc         DWORD ?
+    fErase      DWORD ?
+    rcPaint     RECT <>
+    fRestore    DWORD ?
+    fIncUpdate  DWORD ?
+    rgbReserved BYTE 32 DUP(?)
+PAINTSTRUCT ENDS
+
+GetModuleHandleA PROTO :DWORD
+LoadCursorA      PROTO :DWORD, :DWORD
+RegisterClassA   PROTO :DWORD
+CreateWindowExA  PROTO :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD
+ShowWindow       PROTO :DWORD, :DWORD
+UpdateWindow     PROTO :DWORD
+GetMessageA      PROTO :DWORD, :DWORD, :DWORD, :DWORD
+TranslateMessage PROTO :DWORD
+DispatchMessageA PROTO :DWORD
+DefWindowProcA   PROTO :DWORD, :DWORD, :DWORD, :DWORD
+DestroyWindow    PROTO :DWORD
+PostQuitMessage  PROTO :DWORD
+SetTimer         PROTO :DWORD, :DWORD, :DWORD, :DWORD
+KillTimer        PROTO :DWORD, :DWORD
+InvalidateRect   PROTO :DWORD, :DWORD, :DWORD
+
+BeginPaint       PROTO :DWORD, :DWORD
+EndPaint         PROTO :DWORD, :DWORD
+FillRect         PROTO :DWORD, :DWORD, :DWORD
+GetStockObject   PROTO :DWORD
+CreatePen        PROTO :DWORD, :DWORD, :DWORD
+SelectObject     PROTO :DWORD, :DWORD
+DeleteObject     PROTO :DWORD
+SetBkMode        PROTO :DWORD, :DWORD
+SetTextColor     PROTO :DWORD, :DWORD
+TextOutA         PROTO :DWORD, :DWORD, :DWORD, :DWORD, :DWORD
+MoveToEx         PROTO :DWORD, :DWORD, :DWORD, :DWORD
+LineTo           PROTO :DWORD, :DWORD, :DWORD
+
+wsprintfA        PROTO C :DWORD, :DWORD, :VARARG
+ExitProcess      PROTO :DWORD
+
+GetModuleHandle  TEXTEQU <GetModuleHandleA>
+LoadCursor       TEXTEQU <LoadCursorA>
+RegisterClass    TEXTEQU <RegisterClassA>
+CreateWindowEx   TEXTEQU <CreateWindowExA>
+GetMessage       TEXTEQU <GetMessageA>
+DispatchMessage  TEXTEQU <DispatchMessageA>
+DefWindowProc    TEXTEQU <DefWindowProcA>
+TextOut          TEXTEQU <TextOutA>
+wsprintf         TEXTEQU <wsprintfA>
 
 ; Forward declarations for later Win32 code.
 WinMain PROTO,
@@ -87,6 +200,13 @@ FireShot PROTO
 SpawnEnemy PROTO
 ; CheckShotEnemyCollision will compare active shots to active enemies.
 CheckShotEnemyCollision PROTO
+GetTickCount PROTO
+
+LerpLanePoint PROTO,
+    lane:DWORD,
+    depth:DWORD,
+    pX:DWORD,
+    pY:DWORD
 
 .data
 className   BYTE "MASMTempestWindow",0
@@ -107,6 +227,7 @@ enemyActive BYTE  MAX_ENEMIES DUP(0)
 enemyLane   DWORD MAX_ENEMIES DUP(0)
 enemyDepth  DWORD MAX_ENEMIES DUP(0)
 enemySpawnTick DWORD 0
+enemyNextLane DWORD 0
 score       DWORD START_SCORE
 lives       DWORD INITIAL_LIVES
 gameState   DWORD STATE_TITLE
@@ -168,6 +289,24 @@ farYPoints  DWORD 210, 222, 255, 300, 345, 378
 ; The smaller ring leaves room for inward lane lines.
 ; Drawing code can reuse the same loop bounds for both rings.
 ; Later loops can step through both arrays with one lane index.
+
+drawIndex DWORD 0
+drawX     DWORD 0
+drawY     DWORD 0
+
+tunnelPen  DWORD 0
+drawX2     DWORD 0
+drawY2     DWORD 0
+
+drawDepth DWORD 0
+drawLane1 DWORD 0
+drawLane2 DWORD 0
+enemyX1   DWORD 0
+enemyY1   DWORD 0
+enemyX2   DWORD 0
+enemyY2   DWORD 0
+
+currentSpawnTicks DWORD SPAWN_TICKS
 
 .code
 main PROC
@@ -247,13 +386,25 @@ WndProc PROC,
     ; DrawPlayer will sit on top of the tunnel geometry.
     INVOKE DrawPlayer, paintData.hdc
     ; Use one bright pen for shot and enemy overlay lines.
-    INVOKE CreatePen, PS_SOLID, 1, 0000FFFFh
+    ; Draw shots in red so they do not blend into the tunnel.
+    INVOKE CreatePen, PS_SOLID, 2, SHOT_COLOR
     mov testPen, eax
     INVOKE SelectObject, paintData.hdc, testPen
     mov oldPen, eax
-    ; The moving objects now own this overlay pen work.
+
     call DrawShots
+
+    INVOKE SelectObject, paintData.hdc, oldPen
+    INVOKE DeleteObject, testPen
+
+    ; Draw enemies separately in yellow.
+    INVOKE CreatePen, PS_SOLID, 2, ENEMY_COLOR
+    mov testPen, eax
+    INVOKE SelectObject, paintData.hdc, testPen
+    mov oldPen, eax
+
     call DrawEnemies
+
     INVOKE SelectObject, paintData.hdc, oldPen
     INVOKE DeleteObject, testPen
     ; Draw the HUD and state prompts with a transparent text background.
@@ -345,6 +496,7 @@ check_play_fire:
     jne check_enter
 fire_shot:
     INVOKE FireShot
+    INVOKE InvalidateRect, hWnd, 0, 1
     xor eax, eax
     ret
 check_enter:
@@ -356,6 +508,8 @@ check_enter:
     mov lives, INITIAL_LIVES
     mov playerLane, PLAYER_START_LANE
     mov enemySpawnTick, 0
+    mov currentSpawnTicks, SPAWN_TICKS
+    mov enemyNextLane, 0
     mov gameState, STATE_PLAYING
     mov ecx, MAX_SHOTS
 reset_slots:
@@ -399,95 +553,299 @@ WndProc ENDP
 ; DrawTunnel traces both rings and the lane connectors.
 DrawTunnel PROC,
     hdc:DWORD
-    ; Start at the top of the near ring.
-    INVOKE MoveToEx, hdc, DWORD PTR nearXPoints[0], DWORD PTR nearYPoints[0], 0
-    mov ecx, 1
+
+    INVOKE CreatePen, PS_SOLID, 1, 0000FFFFh
+    mov tunnelPen, eax
+
+    INVOKE SelectObject, hdc, tunnelPen
+    mov oldPen, eax
+
+    ; Draw near outer ring.
+    INVOKE MoveToEx, hdc, nearXPoints[0], nearYPoints[0], 0
+
+    mov drawIndex, 1
+
 draw_near_ring:
-    INVOKE LineTo, hdc, DWORD PTR nearXPoints[ecx*4], DWORD PTR nearYPoints[ecx*4]
-    inc ecx
-    cmp ecx, LANE_COUNT
+    mov eax, drawIndex
+    mov ebx, nearXPoints[eax*4]
+    mov edx, nearYPoints[eax*4]
+
+    INVOKE LineTo, hdc, ebx, edx
+
+    inc drawIndex
+    cmp drawIndex, LANE_COUNT
     jb draw_near_ring
-    ; Close the polygon by returning to the first lane.
-    INVOKE LineTo, hdc, DWORD PTR nearXPoints[0], DWORD PTR nearYPoints[0]
-    ; Trace the smaller inner ring with the same lane order.
-    INVOKE MoveToEx, hdc, DWORD PTR farXPoints[0], DWORD PTR farYPoints[0], 0
-    mov ecx, 1
+
+    INVOKE LineTo, hdc, nearXPoints[0], nearYPoints[0]
+
+    ; Draw far inner ring.
+    INVOKE MoveToEx, hdc, farXPoints[0], farYPoints[0], 0
+
+    mov drawIndex, 1
+
 draw_far_ring:
-    INVOKE LineTo, hdc, DWORD PTR farXPoints[ecx*4], DWORD PTR farYPoints[ecx*4]
-    inc ecx
-    cmp ecx, LANE_COUNT
+    mov eax, drawIndex
+    mov ebx, farXPoints[eax*4]
+    mov edx, farYPoints[eax*4]
+
+    INVOKE LineTo, hdc, ebx, edx
+
+    inc drawIndex
+    cmp drawIndex, LANE_COUNT
     jb draw_far_ring
-    INVOKE LineTo, hdc, DWORD PTR farXPoints[0], DWORD PTR farYPoints[0]
-    ; Draw lane connectors from the outer ring inward.
-    mov ecx, 0
+
+    INVOKE LineTo, hdc, farXPoints[0], farYPoints[0]
+
+    ; Draw lane connector lines.
+    mov drawIndex, 0
+
 draw_lane_lines:
-    INVOKE MoveToEx, hdc, DWORD PTR nearXPoints[ecx*4], DWORD PTR nearYPoints[ecx*4], 0
-    INVOKE LineTo, hdc, DWORD PTR farXPoints[ecx*4], DWORD PTR farYPoints[ecx*4]
-    inc ecx
-    cmp ecx, LANE_COUNT
+    mov eax, drawIndex
+
+    mov ebx, nearXPoints[eax*4]
+    mov edx, nearYPoints[eax*4]
+    INVOKE MoveToEx, hdc, ebx, edx, 0
+
+    mov eax, drawIndex
+    mov ebx, farXPoints[eax*4]
+    mov edx, farYPoints[eax*4]
+    INVOKE LineTo, hdc, ebx, edx
+
+    inc drawIndex
+    cmp drawIndex, LANE_COUNT
     jb draw_lane_lines
+
+    INVOKE SelectObject, hdc, oldPen
+    INVOKE DeleteObject, tunnelPen
+
     ret
 DrawTunnel ENDP
 
-; DrawPlayer renders a small marker on the selected near lane.
 DrawPlayer PROC,
     hdc:DWORD
+
     INVOKE CreatePen, PS_SOLID, PLAYER_PEN_WIDTH, PLAYER_COLOR
     mov testPen, eax
+
     INVOKE SelectObject, hdc, testPen
     mov oldPen, eax
-    mov eax, playerLane
-    mov ecx, DWORD PTR nearXPoints[eax*4]
-    mov edx, DWORD PTR nearYPoints[eax*4]
-    sub edx, PLAYER_MARKER_SIZE
-    INVOKE MoveToEx, hdc, ecx, edx, 0
-    add edx, PLAYER_MARKER_SIZE * 2
-    INVOKE LineTo, hdc, ecx, edx
+
+    ; Outer point of player.
+    INVOKE LerpLanePoint, playerLane, 0, ADDR drawX, ADDR drawY
+
+    ; Inner point of player, slightly toward the center.
+    INVOKE LerpLanePoint, playerLane, 16, ADDR drawX2, ADDR drawY2
+
+    INVOKE MoveToEx, hdc, drawX, drawY, 0
+    INVOKE LineTo, hdc, drawX2, drawY2
+
     INVOKE SelectObject, hdc, oldPen
     INVOKE DeleteObject, testPen
+
     ret
 DrawPlayer ENDP
 
 DrawShots PROC
-    mov ecx, 0
+
+    mov drawIndex, 0
+
 draw_shots:
-    cmp ecx, MAX_SHOTS
+    cmp drawIndex, MAX_SHOTS
     jae draw_shots_done
-    cmp BYTE PTR shotActive[ecx], 0
+
+    mov eax, drawIndex
+    cmp BYTE PTR shotActive[eax], 0
     je next_draw_shot
-    mov eax, DWORD PTR shotLane[ecx*4]
-    mov edx, DWORD PTR nearYPoints[eax*4]
-    sub edx, DWORD PTR shotDepth[ecx*4]
-    INVOKE MoveToEx, paintData.hdc, DWORD PTR nearXPoints[eax*4], edx, 0
-    add edx, 8
-    INVOKE LineTo, paintData.hdc, DWORD PTR nearXPoints[eax*4], edx
+
+    mov eax, drawIndex
+    mov ebx, shotLane[eax*4]
+
+    ; First point of shot.
+    mov eax, drawIndex
+    INVOKE LerpLanePoint, ebx, shotDepth[eax*4], ADDR drawX, ADDR drawY
+
+    ; Second point of shot, slightly behind it.
+    mov eax, drawIndex
+    mov edx, shotDepth[eax*4]
+    sub edx, 10
+    cmp edx, 0
+    jge shot_depth_ok
+    mov edx, 0
+
+shot_depth_ok:
+    INVOKE LerpLanePoint, ebx, edx, ADDR drawX2, ADDR drawY2
+
+    INVOKE MoveToEx, paintData.hdc, drawX, drawY, 0
+    INVOKE LineTo, paintData.hdc, drawX2, drawY2
+
 next_draw_shot:
-    inc ecx
+    inc drawIndex
     jmp draw_shots
+
 draw_shots_done:
     ret
+
 DrawShots ENDP
+
 DrawEnemies PROC
-    mov ecx, 0
+
+    mov drawIndex, 0
+
 draw_enemies:
-    cmp ecx, MAX_ENEMIES
+    cmp drawIndex, MAX_ENEMIES
     jae draw_enemies_done
-    cmp BYTE PTR enemyActive[ecx], 0
+
+    mov eax, drawIndex
+    cmp BYTE PTR enemyActive[eax], 0
     je next_draw_enemy
-    mov eax, DWORD PTR enemyLane[ecx*4]
-    mov edx, DWORD PTR farYPoints[eax*4]
-    add edx, DWORD PTR enemyDepth[ecx*4]
-    INVOKE MoveToEx, paintData.hdc, DWORD PTR farXPoints[eax*4], edx, 0
-    sub edx, 8
-    INVOKE LineTo, paintData.hdc, DWORD PTR farXPoints[eax*4], edx
+
+    ; ebx = enemy lane
+    mov eax, drawIndex
+    mov ebx, enemyLane[eax*4]
+
+    ; edx = interpolation depth.
+    ; enemyDepth grows outward, so convert it to lane interpolation depth.
+    mov eax, drawIndex
+    mov edx, DEPTH_RANGE
+    sub edx, enemyDepth[eax*4]
+
+    cmp edx, 0
+    jge enemy_depth_ok
+    mov edx, 0
+
+enemy_depth_ok:
+    ; Save the enemy depth.
+    mov drawDepth, edx
+
+    ; left neighbor lane = lane - 1, wrapped.
+    mov eax, ebx
+    cmp eax, 0
+    jne enemy_left_normal
+    mov eax, LANE_COUNT
+
+enemy_left_normal:
+    dec eax
+    mov drawLane1, eax
+
+    ; right neighbor lane = lane + 1, wrapped.
+    mov eax, ebx
+    inc eax
+    cmp eax, LANE_COUNT
+    jb enemy_right_ok
+    mov eax, 0
+
+enemy_right_ok:
+    mov drawLane2, eax
+
+    ; Get point on left neighboring lane at same depth.
+    INVOKE LerpLanePoint, drawLane1, drawDepth, ADDR drawX, ADDR drawY
+
+    ; Get point on right neighboring lane at same depth.
+    INVOKE LerpLanePoint, drawLane2, drawDepth, ADDR drawX2, ADDR drawY2
+
+    ; Draw a shortened cross-line between those two lane points.
+    ; New start = left + 3/8 of the way to right.
+    mov eax, drawX2
+    sub eax, drawX
+    imul eax, 3
+    cdq
+    mov ecx, 8
+    idiv ecx
+    add eax, drawX
+    mov enemyX1, eax
+
+    mov eax, drawY2
+    sub eax, drawY
+    imul eax, 3
+    cdq
+    mov ecx, 8
+    idiv ecx
+    add eax, drawY
+    mov enemyY1, eax
+
+    ; New end = left + 5/8 of the way to right.
+    mov eax, drawX2
+    sub eax, drawX
+    imul eax, 5
+    cdq
+    mov ecx, 8
+    idiv ecx
+    add eax, drawX
+    mov enemyX2, eax
+
+    mov eax, drawY2
+    sub eax, drawY
+    imul eax, 5
+    cdq
+    mov ecx, 8
+    idiv ecx
+    add eax, drawY
+    mov enemyY2, eax
+
+    INVOKE MoveToEx, paintData.hdc, enemyX1, enemyY1, 0
+    INVOKE LineTo, paintData.hdc, enemyX2, enemyY2
+
 next_draw_enemy:
-    inc ecx
+    inc drawIndex
     jmp draw_enemies
+
 draw_enemies_done:
     ret
+
 DrawEnemies ENDP
+
+LerpLanePoint PROC USES ebx esi edi,
+    lane:DWORD,
+    depth:DWORD,
+    pX:DWORD,
+    pY:DWORD
+
+    ; Clamp depth to DEPTH_RANGE.
+    mov eax, depth
+    cmp eax, DEPTH_RANGE
+    jle depth_not_too_large
+    mov eax, DEPTH_RANGE
+
+depth_not_too_large:
+    cmp eax, 0
+    jge depth_ok
+    mov eax, 0
+
+depth_ok:
+    mov depth, eax
+
+    mov esi, lane
+
+    ; x = nearX + ((farX - nearX) * depth) / DEPTH_RANGE
+    mov eax, farXPoints[esi*4]
+    sub eax, nearXPoints[esi*4]
+    imul depth
+    cdq
+    mov ebx, DEPTH_RANGE
+    idiv ebx
+    add eax, nearXPoints[esi*4]
+
+    mov edi, pX
+    mov DWORD PTR [edi], eax
+
+    ; y = nearY + ((farY - nearY) * depth) / DEPTH_RANGE
+    mov eax, farYPoints[esi*4]
+    sub eax, nearYPoints[esi*4]
+    imul depth
+    cdq
+    mov ebx, DEPTH_RANGE
+    idiv ebx
+    add eax, nearYPoints[esi*4]
+
+    mov edi, pY
+    mov DWORD PTR [edi], eax
+
+    ret
+
+LerpLanePoint ENDP
+
 ; UpdateGame advances live gameplay state only during active play.
-UpdateGame PROC
+UpdateGame PROC USES ebx
     ; Freeze movement, spawning, and collisions outside active play.
     ; Title, pause, and game-over all share this early return.
     ; Reset the spawn counter so resume does not burst-spawn.
@@ -497,19 +855,52 @@ UpdateGame PROC
     ret
 update_live:
     ; Count update ticks before spawning another enemy.
+        ; Recalculate spawn delay from score.
+    ; 0-999 score     = 240 ticks
+    ; 1000-1999 score = 200 ticks
+    ; 2000-2999 score = 160 ticks
+    ; 3000-3999 score = 120 ticks
+    ; 4000-4999 score = 80 ticks
+    ; 5000+ score     = 40 ticks
+
+    mov eax, score
+    xor edx, edx
+    mov ebx, SPAWN_STEP_SCORE
+    div ebx                     ; eax = score / 1000
+
+    mov ebx, SPAWN_STEP_TICKS
+    mul ebx                     ; eax = scoreStep * 40
+
+    mov ebx, SPAWN_TICKS
+    sub ebx, eax                ; ebx = 240 - scoreStep*40
+
+    cmp ebx, MIN_SPAWN_TICKS
+    jae spawn_delay_ok
+    mov ebx, MIN_SPAWN_TICKS
+
+spawn_delay_ok:
+    mov currentSpawnTicks, ebx
+
     inc enemySpawnTick
-    cmp enemySpawnTick, SPAWN_TICKS
-    jb update_shots
+    mov eax, currentSpawnTicks
+    cmp enemySpawnTick, eax
+    jb no_spawn_this_tick
+
     mov enemySpawnTick, 0
     call SpawnEnemy
+
+no_spawn_this_tick:
+    ; Always reset ECX before updating shots.
+    ; Otherwise shots only update reliably on enemy-spawn frames.
     mov ecx, 0
+
 update_shots:
     cmp ecx, MAX_SHOTS
     jae update_enemies
     cmp BYTE PTR shotActive[ecx], 0
     je next_shot
     add DWORD PTR shotDepth[ecx*4], 12
-    cmp DWORD PTR shotDepth[ecx*4], NEAR_RADIUS
+    cmp DWORD PTR shotDepth[ecx*4], DEPTH_RANGE
     jbe next_shot
     mov BYTE PTR shotActive[ecx], 0
 next_shot:
@@ -522,23 +913,22 @@ move_enemies:
     jae update_done
     cmp BYTE PTR enemyActive[ecx], 0
     je next_enemy
-    add DWORD PTR enemyDepth[ecx*4], 6
-    cmp DWORD PTR enemyDepth[ecx*4], NEAR_RADIUS
+    add DWORD PTR enemyDepth[ecx*4], 1
+    cmp DWORD PTR enemyDepth[ecx*4], DEPTH_RANGE
     jb next_enemy
-    ; Remove enemies that reach the player edge.
-    mov eax, DWORD PTR enemyLane[ecx*4]
-    cmp eax, playerLane
-    jne clear_enemy
-    ; The final life flips the game into the game-over state.
+
+    ; Any enemy that reaches the outer shell costs one life.
     cmp DWORD PTR lives, 1
-    ja lose_life
+    ja lose_life_any_lane
+
     mov DWORD PTR lives, 0
     mov gameState, STATE_GAME_OVER
-    jmp clear_enemy
-lose_life:
-    ; Earlier hits only reduce the remaining life count.
+    jmp clear_enemy_any_lane
+
+lose_life_any_lane:
     dec DWORD PTR lives
-clear_enemy:
+
+clear_enemy_any_lane:
     mov BYTE PTR enemyActive[ecx], 0
 next_enemy:
     inc ecx
@@ -568,19 +958,31 @@ fire_done:
     ret
 FireShot ENDP
 
-SpawnEnemy PROC
+SpawnEnemy PROC USES ebx edx
     mov ecx, 0
+
 find_free_enemy:
     cmp ecx, MAX_ENEMIES
     jae spawn_done
+
     cmp BYTE PTR enemyActive[ecx], 0
     je activate_enemy
+
     inc ecx
     jmp find_free_enemy
+
 activate_enemy:
     mov BYTE PTR enemyActive[ecx], 1
-    mov DWORD PTR enemyLane[ecx*4], 3
+
+    ; Pick a lane from 0 to 11 using GetTickCount.
+    INVOKE GetTickCount
+    xor edx, edx
+    mov ebx, LANE_COUNT
+    div ebx                     ; edx = remainder 0..LANE_COUNT-1
+
+    mov DWORD PTR enemyLane[ecx*4], edx
     mov DWORD PTR enemyDepth[ecx*4], 0
+
 spawn_done:
     ret
 SpawnEnemy ENDP
@@ -599,7 +1001,7 @@ check_collision_slot:
     jne next_collision_slot
     mov eax, DWORD PTR shotDepth[ecx*4]
     add eax, DWORD PTR enemyDepth[ecx*4]
-    cmp eax, NEAR_RADIUS - FAR_RADIUS
+    cmp eax, DEPTH_RANGE
     jb next_collision_slot
     ; Clear the matching slots and award the hit score.
     ; The current test still compares same-index shot and enemy entries.
